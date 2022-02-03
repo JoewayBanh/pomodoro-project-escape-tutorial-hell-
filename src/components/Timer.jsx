2bab7c61b65e8d350a31dd00267416113d37257d
Joeway Banh <joewaybanh@hotmail.com>
2022-02-03 20:48:28 +0100

    added functionality for taking short breaks

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,6 +5,8 @@ const Timer = () => {
   const [timerStarted, setTimerStarted] = useState(false);
   const [completedPoms, setCompletedPoms] = useState(0);
   const [pomsBeforeLongBreak, setPomsBeforeLongBreak] = useState(4);
+  const [isBreakTime, setIsBreakTime] = useState(null);
+  const [startLongBreak, setStartLongBreak] = useState(false);
 
   useEffect(() => {
     if (timer !== 0 && timerStarted) {
@@ -12,10 +14,17 @@ const Timer = () => {
         setTimer(timer - 2);
       }, 500);
     }
-    if (timer === 0 && pomsBeforeLongBreak !== 0) {
+    if (timer === 0 && !isBreakTime) {
+      setTimer(6);
+      setTimerStarted(false);
+      setIsBreakTime(true);
       setPomsBeforeLongBreak(pomsBeforeLongBreak - 1);
       setCompletedPoms(completedPoms + 1);
+    }
+    if (timer === 0 && pomsBeforeLongBreak % 4 !== 0 && isBreakTime) {
       setTimer(10);
+      setTimerStarted(false);
+      setIsBreakTime(false);
     }
   }, [timer, timerStarted]);
 
@@ -29,6 +38,11 @@ const Timer = () => {
       <button onClick={startTimerHandler}>Start timer!</button>
       <p>{completedPoms} completed.</p>
       <p>{pomsBeforeLongBreak} before long break</p>
+      <br />
+      {!isBreakTime && <p>Click to start pomodoro timer</p>}
+      {isBreakTime && <p>Click to start break timer</p>}
+      <br />
+      <p>Poms before long break: {pomsBeforeLongBreak}</p>
     </div>
   );
 };
